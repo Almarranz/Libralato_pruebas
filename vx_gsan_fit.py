406cf5abf734dda9deb99c488a5bcdbef55b61db
Alvaro Martinez <amartinez@iaa.es>
2022-02-09 22:38:48 +0100

    Frist aproach

--- vx_gsan_fit.py
+++ vx_gsan_fit.py
@@ -99,7 +99,7 @@ n1=n1[good]
 n2=n2[good]
 idt=idt[good]
 #%%
-perc_dmul= np.percentile(dmul,50)
+perc_dmul= np.percentile(dmul,85)
 print(perc_dmul,'yomama')
 # lim_dmul=perc_dmul
 lim_dmul=1
@@ -113,103 +113,108 @@ time=time[accu]
 #%%
 print(min(mul),max(mul))
 
-auto='auto'
+auto='no'
 if auto !='auto':
     auto=np.arange(min(mul),max(mul),0.25)#also works if running each bing width one by one, for some reason...
     # print(auto)
 
 #%%
 
-
-#%%
 fig, ax = plt.subplots(1,1, figsize=(10,10))
 
-# sig_hb=sigma_clip(mub,sigma=500,maxiters=20,cenfunc='mean',masked=True)
-# mub=mub[sig_hb.mask==False]
+# sig_h=sigma_clip(mul,sigma=500,maxiters=20,cenfunc='mean',masked=True)
+# mul=mul[sig_h.mask==False]
 
-hb=ax.hist(mub,bins=auto,color='orange',linewidth=2,density=True)
-hb1=np.histogram(mub,bins=auto,density=False)
+h=ax.hist(mul,bins=auto,linewidth=2,density=True)
+h1=np.histogram(mul,bins=auto,density=False)
 
-xb=[hb[1][i]+(hb[1][1]-hb[1][0])/2 for i in range(len(hb[0]))]#middle value for each bin
-ax.axvline(np.mean(mub), color='r', linestyle='dashed', linewidth=3)
+x=[h[1][i]+(h[1][1]-h[1][0])/2 for i in range(len(h[0]))]#middle value for each bin
+ax.axvline(np.mean(mul), color='r', linestyle='dashed', linewidth=3)
 ax.legend(['List=%s, %s, mean= %.2f, std=%.2f'
-                  %(name,len(mub),np.mean(mub),np.std(mub))],fontsize=12,markerscale=0,shadow=True,loc=1,handlelength=-0.0)
+                  %(name,len(mul),np.mean(mul),np.std(mul))],fontsize=12,markerscale=0,shadow=True,loc=1,handlelength=-0.0)
 ax.set_ylabel('N')
-ax.set_xlim(-10,10)
-ax.set_xlabel(r'$\mathrm{\mu_{b} (mas\ yr^{-1})}$') 
-yb=hb[0]#height for each bin
+ax.set_xlim(-13,3)
+ax.set_xlabel(r'$\mathrm{\mu_{l} (mas\ yr^{-1})}$') 
+ax.invert_xaxis()
+y=h[0]#height for each bin
+
 #%%
 
-# =============================================================================
-# fig, ax =plt.subplots(1,1,figsize=(10,10))
-# ax.scatter(mul,mub,color='k',s=1,alpha=0.05)
-# ax.set_xlim(-13,2)
-# ax.set_ylim(-10,10)
-# ax.axvline(0)
-# ax.axhline(0)
-# ax.axhline(-0.22)
-# ax.invert_xaxis()
-# 
-# =============================================================================
-# %%
+#%%
+
+fig, ax =plt.subplots(1,1,figsize=(10,10))
+ax.scatter(mul,mub,color='k',s=1,alpha=0.05)
+ax.set_xlim(-13,2)
+ax.set_ylim(-10,10)
+ax.axvline(0)
+ax.axhline(0)
+ax.axhline(-0.22)
+ax.invert_xaxis()
+
+#%%
 
 yerr=[]
-yb=np.where(yb==0,0.001,yb)
-y1=hb1[0]
+y=np.where(y==0,0.001,y)
+y1=h1[0]
 y1=np.where(y1==0,0.001,y1)
-yerr = yb*np.sqrt(1/y1)
-# yerr = y*np.sqrt(1/y1+1/len(v_y))
+# yerr = y*np.sqrt(1/y1+1/len(v_x))
+yerr = y*np.sqrt(1/y1)
+# %%
 
-    
 # In[7]:   
 def gaussian(x, mu, sig, amp):
     return amp * (1 / (sig * (np.sqrt(2 * np.pi)))) * np.exp(-np.power(x - mu, 2.) / (2 * np.power(sig, 2.))) 
 #%%
 def loglike(theta):
-    mu1, sigma1, amp1,mu2,sigma2,amp2 = theta
-    model = gaussian(xb, mu1, sigma1, amp1)+gaussian(xb,mu2,sigma2,amp2)
+    mu1, sigma1, amp1,mu2,sigma2,amp2,mu3,sigma3,amp3 = theta
+    model = gaussian(x, mu1, sigma1, amp1)+gaussian(x,mu2,sigma2,amp2)+gaussian(x,mu3,sigma3,amp3)
      
-    return -0.5 * np.sum(((yb - model)/yerr) ** 2)#chi squared model
-#%% 
+    return -0.5 * np.sum(((y - model)/yerr) ** 2)#chi squared model
+
+# %%
+
 def prior_transform(utheta):
     """Transforms the uniform random variable `u ~ Unif[0., 1.)`
     to the parameter of interest `x ~ Unif[-10., 10.)`."""
     #x = 2. * u - 1.  # scale and shift to [-1., 1.)
     #x *= 10.  # scale to [-10., 10.)
-    umu1, usigma1, uamp1,  umu2, usigma2, uamp2= utheta
-
+    umu1, usigma1, uamp1,  umu2, usigma2, uamp2, umu3, usigma3, uamp3= utheta
+     
 #     mu1 = -1. * umu1-8   # scale and shift to [-10., 10.)
-    mu1 = 1* umu1-0.5  # scale and shift to [-10., 10.)
-    sigma1 = 5* (usigma1)   
+    mu1 = -3+ (4*umu1-4/2)  # scale and shift to [-10., 10.)
+    sigma1 = 4* (usigma1)   
     amp1 = 1 * uamp1 
-
-    
-    mu2 = 2 * umu2-1
-    sigma2 = 2 * usigma2   
+   
+    mu2 = -5+ (6*umu2-6/2)
+    sigma2 = 4 * usigma2   
     amp2 = 1* uamp2   
-    
 
-    return mu1, sigma1, amp1, mu2, sigma2, amp2
+    mu3 = -7+ (4*umu3-4/2) # scale and shift to [-3., 3.)
+    sigma3 = 3.3*(usigma3)
+    amp3 = uamp3
+        
+    return mu1, sigma1, amp1, mu2, sigma2, amp2, mu3, sigma3, amp3
+    
 #%% 
-sampler = dynesty.NestedSampler(loglike, prior_transform, ndim=6, nlive=500,
+sampler = dynesty.NestedSampler(loglike, prior_transform, ndim=9, nlive=200,
                                             bound='multi', sample='rwalk')
 sampler.run_nested()
 res = sampler.results
-#%%
+
+# %%
 from dynesty import plotting as dyplot
-rcParams.update({'font.size': 10})
-# truths = [mu1_true, sigma1_true, amp1_true, mu2_true, sigma2_true, amp2_true]
-labels = [r'$\mathrm{\mu 1}$', r'$\mathrm{\sigma 1}$', r'$amp1$', r'$\mathrm{\mu 2}$', r'$\mathrm{\sigma 2}$', r'$amp2$']
-# fig, axes = dyplot.traceplot(sampler.results, truths=truths, labels=labels,
-#      $\mathrm{\mu_{b}}                        fig=plt.subplots(6, 2, figsize=(16, 27)))
 
+labels = [r'$\mathrm{\mu 1}$', r'$\mathrm{\sigma 1}$', r'$amp1$', r'$\mathrm{\mu 2}$', r'$\mathrm{\sigma 2}$', r'$amp2$', 
+              r'$\mathrm{\mu 3}$', r'$\mathrm{\sigma 3}$', r'$amp3$']
+    # fig, axes = dyplot.traceplot(sampler.results, truths=truths, labels=labels,
+    #                              fig=plt.subplots(6, 2, figsize=(16, 27)))
+    
 fig, axes = dyplot.traceplot(sampler.results,labels=labels,show_titles=True,
-                             fig=plt.subplots(6, 2, figsize=(20, 16)))
-
-
-
+                                 fig=plt.subplots(9, 2, figsize=(16, 20)))
+    
 plt.show()
-rcParams.update({'font.size': 20})
+
+#%%
 
 #%%
 from dynesty import utils as dyfunc
@@ -217,55 +222,88 @@ from dynesty import utils as dyfunc
 samples, weights = res.samples, np.exp(res.logwt - res.logz[-1])
 mean, cov = dyfunc.mean_and_cov(samples, weights)
 print(mean)
-     #%%                              fig=plt.subplots(6, 6, figsize=(28, 28)))
-    # This is de corner plot
-fig, axes = dyplot.cornerplot(res, color='royalblue', show_titles=True, quantiles=[0.16,0.5,0.68],truths=mean,
-                              quantiles_2d=[0.16,0.5,0.68],
-                              title_kwargs={'x': 0.65, 'y': 1.05}, labels=labels,
-                              fig=plt.subplots(6, 6, figsize=(28, 28)))
-plt.legend(['$\mu_{b}$ %s '%(name)],fontsize=70,markerscale=0,shadow=True,bbox_to_anchor=(1,6.5),handlelength=-0.0)
-
-plt.show() 
-    
 # %%
+sp=np.ones(9)*0.68
+fig, axes = dyplot.cornerplot(res, color='royalblue' ,show_titles=True,quantiles=[0.16,0.5,0.84],truths=mean,
+                                  quantiles_2d=[0.16,0.5,0.84],
+                                  title_kwargs=({'x': 0.65, 'y': 1.05}), labels=labels,
+                                  fig=plt.subplots(9, 9, figsize=(28, 28)))
 
-from matplotlib import rc
-rc('font',**{'family':'serif','serif':['Palatino']})
-
+plt.legend(['$\mu_{l}$ %s'%(name)],fontsize=70,markerscale=0,shadow=True,bbox_to_anchor=(1.2,9.8),handlelength=-0.0)
+plt.show() 
+# %%   
 
 results = sampler.results
 print(results['logz'][-1])
 
-fig, ax = plt.subplots(figsize=(8,8))
-ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-h=ax.hist(mub, bins= auto, color='royalblue', alpha = 0.6, density =True, histtype = 'stepfilled')
+# h=plt.hist(v_x*-1, bins= nbins, color='darkblue', alpha = 0.6, density =True, histtype = 'stepfilled')
+h=plt.hist(mul, bins= auto, color='darkblue', alpha = 0.6, density =True, histtype = 'stepfilled')
 
-
-xplot = np.linspace(min(xb), max(xb), 1000)
+xplot = np.linspace(min(x), max(x), 500)
 
 # plt.plot(xplot, gaussian(xplot, mean[0], mean[1], mean[2]) , color="darkorange", linewidth=3, alpha=0.6)
 
-plt.plot(xplot, gaussian(xplot, mean[0], mean[1], mean[2]) + gaussian(xplot, mean[3], mean[4], mean[5]), color="darkorange", linewidth=3, alpha=1)
-plt.plot(xplot, gaussian(xplot, mean[0], mean[1], mean[2])  , color="red", linestyle='dashed', linewidth=3, alpha=0.6)
-plt.plot(xplot, gaussian(xplot, mean[3], mean[4], mean[5])  , color="k", linestyle='dashed', linewidth=3, alpha=0.6)
+plt.plot(xplot, gaussian(xplot, mean[0], mean[1], mean[2]) + gaussian(xplot, mean[3], mean[4], mean[5])
+         + gaussian(xplot, mean[6], mean[7], mean[8]), color="darkorange", linewidth=3, alpha=1)
+plt.plot(xplot, gaussian(xplot, mean[0], mean[1], mean[2])  , color="yellow", linestyle='dashed', linewidth=3, alpha=0.6)
+plt.plot(xplot, gaussian(xplot, mean[3], mean[4], mean[5])  , color="red", linestyle='dashed', linewidth=3, alpha=0.6)
+plt.plot(xplot, gaussian(xplot, mean[6], mean[7], mean[8]) , color='black', linestyle='dashed', linewidth=3, alpha=0.6)
+plt.xlim(-15,3)
+plt.text(-10,max(h[0]-0.01),'logz=%.0f'%(results['logz'][-1]),color='b')
 
-plt.text(5,max(hb[0]-0.05),'$logz=%.0f$'%(results['logz'][-1]),color='b')
-
-plt.xlim(-15,15)
+plt.gca().invert_xaxis()
 
 plt.ylabel('N')
-plt.xlabel(r'$\mathrm{\mu_{b} (mas\ yr^{-1})}$')
-# %%
-samples, weights = res.samples, np.exp(res.logwt - res.logz[-1])
-mean, cov = dyfunc.mean_and_cov(samples, weights)
-# print(mean)
-quantiles = [dyfunc.quantile(samps, [0.16,0.5,0.84], weights=weights)
-             for samps in samples.T]
+plt.xlabel(r'$\mathrm{\mu_{l} (mas\ yr^{-1})}$') 
 
-for i in range(6):
-    print('medin %.2f -+ %.2f %.2f'%(quantiles[i][1],quantiles[i][1]-quantiles[i][0],quantiles[i][2]-quantiles[i][1]))
-    print(' mean %.2f -+ %.2f %.2f'%(mean[i],mean[i]-quantiles[i][0],quantiles[i][2]-mean[i])+'\n'+30*'*')
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
