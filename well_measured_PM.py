c7d2b1a3c931b7a662737b8fe48295b9f83e4072
Alvaro Martinez <amartinez@iaa.es>
2022-02-16 16:42:00 +0100

    Added list with well mesured pm and photometries

--- well_measured_PM.py
+++ well_measured_PM.py
@@ -53,7 +53,7 @@ pruebas='/Users/amartinez/Desktop/PhD/Libralato_data/pruebas/'
 ep1_fix=np.loadtxt(pruebas+'foto_well_mesaured_ep%s_%s.txt'%(name,1))
 ep2_fix=np.loadtxt(pruebas+'foto_well_mesaured_ep%s_%s.txt'%(name,2))
 mag, rms, qfit, o, RADXS, nf, nu, Localsky, Local_skyrms= np.loadtxt(cata+'GALCEN_%s_GO12915.cat'%(name),unpack=True )
-
+all_ep1= np.loadtxt(cata+'GALCEN_%s_GO12915.cat'%(name),unpack=False)
 
 
 # %%
@@ -62,18 +62,18 @@ ep12,ep12_ind,ep21_ind=np.intersect1d(ep1_fix[:,9],ep2_fix[:,9], return_indices=
 # %%
 # ra,dec,x_c ,y_c,mua,dmua,mud,dmud, time, n1, n2, idt
 pm_wmp=catal[ep12.astype(int)]#pm_wmp stands for proper motion well mesaured photometry
-mag_ep12=mag[ep12.astype(int)]
+all_eps=all_ep1[ep12.astype(int)]
 # Discarted stars with errors equals 99.999mas/yr
 # Discarted stars with velocities >70
 
 velocity=np.sqrt(pm_wmp[:,4]**2+pm_wmp[:,6]**2)
 v_valid=np.where((pm_wmp[:,5]<90) & (velocity<70) )
 pm_wmp=pm_wmp[v_valid]
-mag_ep12=mag_ep12[v_valid]
+all_eps=all_eps[v_valid]
 # %%
 fig, ax = plt.subplots(1,1, figsize=(10,10))
 ax.scatter(mag,catal[:,5],s=0.1,color='k',alpha=1)
-ax.scatter(mag_ep12,pm_wmp[:,5],s=0.1,color='red',alpha=1)
+ax.scatter(all_eps[:,0],pm_wmp[:,5],s=0.1,color='red',alpha=1)
 ax.set_ylim(0,10)
 ax.set_xlim(12,24)
 # %%
@@ -81,9 +81,9 @@ ax.set_xlim(12,24)
 #     (a) Pm uncertainty better that 85th percentile for any given magnitude
 #     (b) PM uncertainty smaller than 0.7 also good, regardless of the percentile
 paso=0.1
-a=np.round(min(mag_ep12))
-b=np.round(max(mag_ep12))
-mag_b=np.digitize(mag_ep12, np.arange(a,b,paso), right=True)
+a=np.round(min(all_eps[:,0]))
+b=np.round(max(all_eps[:,0]))
+mag_b=np.digitize(all_eps[:,0], np.arange(a,b,paso), right=True)
 xy_error=np.sqrt(pm_wmp[:,5]**2 + pm_wmp[:,7]**2)# I have combinned both axis error, I have the impresion thar libralato applies conditions for each axis separetly
 error_valid=[]
 all_sum=[]
@@ -105,14 +105,18 @@ for i in range(min(mag_b),(max(mag_b)+1)):
 print(sum(all_sum),'All after a')
 print(len(error_valid))
 pm_wmp=pm_wmp[error_valid]
+all_eps=all_eps[error_valid]
 print(len(pm_wmp),'Condition b')
 
 # %%
 fig, ax = plt.subplots(1,1, figsize=(10,10))
 ax.scatter(mag,catal[:,5],s=0.1,color='k',alpha=1)
-ax.scatter(mag_ep12[error_valid],pm_wmp[:,5],s=0.1,color='red',alpha=1)
+ax.scatter(all_eps[:,0],pm_wmp[:,5],s=0.1,color='red',alpha=1)
 ax.set_ylim(0,10)
 ax.set_xlim(12,24)
+
+np.savetxt(pruebas+'refined_%s_PM.txt'%(name),pm_wmp)
+np.savetxt(pruebas+'refined_%s_phot.txt'%(name),all_eps)
 
 
 
