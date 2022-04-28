edc6485a9d2771543f4f4923ca3adf8813b6b02b
Alvaro Martinez <amartinez@iaa.es>
2022-04-28 11:27:19 +0200

    Undestandig the xi paramtre

--- OPTICS_quintuplet.py
+++ OPTICS_quintuplet.py
@@ -52,20 +52,20 @@ mu_alpha = -1 * math.sin(math.radians(148.6)) * (c) + math.cos(math.radians(148.
 mu = np.vstack((mu_alpha,mu_delta)).T
 
 # %%
-samples = 5
+samples = 50
 # %%
-X=np.array([mu_alpha,mu_delta]).T
+# X=np.array([mu_alpha,mu_delta]).T
 # %%This are the valuses for the tutorial
-# np.random.seed(0)
-# n_points_per_cluster = 250
+np.random.seed(0)
+n_points_per_cluster = 250
 
-# C1 = [-5, -2] + 0.8 * np.random.randn(n_points_per_cluster, 2)
-# C2 = [4, -1] + 0.1 * np.random.randn(n_points_per_cluster, 2)
-# C3 = [1, -2] + 0.2 * np.random.randn(n_points_per_cluster, 2)
-# C4 = [-2, 3] + 0.3 * np.random.randn(n_points_per_cluster, 2)
-# C5 = [3, -2] + 1.6 * np.random.randn(n_points_per_cluster, 2)
-# C6 = [5, 6] + 2 * np.random.randn(n_points_per_cluster, 2)
-# X = np.vstack((C1, C2, C3, C4, C5, C6))
+C1 = [-5, -2] + 0.8 * np.random.randn(n_points_per_cluster, 2)
+C2 = [4, -1] + 0.1 * np.random.randn(n_points_per_cluster, 2)
+C3 = [1, -2] + 0.2 * np.random.randn(n_points_per_cluster, 2)
+C4 = [-2, 3] + 0.3 * np.random.randn(n_points_per_cluster, 2)
+C5 = [3, -2] + 1.6 * np.random.randn(n_points_per_cluster, 2)
+C6 = [5, 6] + 2 * np.random.randn(n_points_per_cluster, 2)
+X = np.vstack((C1, C2, C3, C4, C5, C6))
 # %%
 
 # clust = OPTICS(min_samples=50, xi=0.05, metric='euclidean').fit(X)
@@ -73,7 +73,7 @@ X_stad = StandardScaler().fit_transform(X)
 # X_stad = X
 
 # clusterer = hdbscan.HDBSCAN(min_cluster_size=samples, min_samples=min_cor,).fit(X_stad)
-clusterer = OPTICS(min_samples=samples, xi=0.05, metric='euclidean').fit(X_stad)
+clusterer = OPTICS(min_samples=samples, xi=0.05,min_cluster_size=0.1, metric='euclidean').fit(X_stad)
 
 
 l=clusterer.labels_
@@ -151,91 +151,109 @@ distances = np.sort(reachability)
 # distances=reachability
 fif, ax =plt.subplots(1,1,figsize=(10,5))
 
-kneedle = KneeLocator(np.arange(0,len(distances)-1,1), distances[0:-1], interp_method = "polynomial",direction="increasing",curve='convex')
-rodilla=round(kneedle.knee_y, 3)
 
-ax.plot(list(range(len(reachability))),distances)
-ax.axhline(round(kneedle.elbow_y, 3),linestyle='dashed',color='k')
-ax.legend(['knee=%s, min=%s'%(round(kneedle.elbow_y, 3),round(min(distances),2))])
+
+ax.plot(list(range(len(reachability))),reachability)
+
 ax.set_xlabel('Points(sorted by cluster)')
 ax.set_ylabel('Reachabilty(epsilon)')
-# print(round(kneedle.knee, 3))
-# print(round(kneedle.elbow, 3))
-# print(round(kneedle.knee_y, 3))
-# print(round(kneedle.elbow_y, 3))
-for klass in range(0, n_clusters+1):
-    print(klass)
+def slope(x1, y1, x2, y2):
+    m = round((y2-y1)/(x2-x1),5)
+    return m
+
+indx_f=20000
+indx_i=1
+plus = 5
+for klass in range(0, n_clusters):
+# for klass in range(0, 2):
+    
     Xko =space[labels==klass]
     Xk =space[np.where(labels==klass)]
     Rk = reachability[np.where(labels == klass)]
-    ax.scatter(Xk, Rk, color=colors[klass], alpha=0.3)
-ax.scatter(space[np.where(labels==-1)], reachability[np.where(labels==-1)],color=colors[-1])
+    print(klass,Xk[1],Rk[1])
+    ax.scatter(Xk, Rk, color=colors[klass], alpha=0.03)
+    try:
+        steep_f = slope(Xk[indx_f],Rk[indx_f],Xk[indx_f+plus],Rk[indx_f+plus])
+        steep_i = slope(Xk[indx_i],Rk[indx_i],Xk[indx_i+plus],Rk[indx_i+plus])
+        ax.annotate('%s'%(steep_i),(Xk[indx_i+plus],Rk[indx_i+plus]),color = colors[klass],weight='bold')
+        ax.annotate('%s'%(steep_f),(Xk[indx_f+plus],Rk[indx_f+plus]),color = colors[klass],weight='bold')
+    except:
+       steep_f = slope(Xk[-2],Rk[-2],Xk[-1],Rk[-1])
+       ax.annotate('%s'%(steep_f),(Xk[-1],Rk[-1]),color = colors[klass],weight='bold')
+
+    
+ax.scatter(space[np.where(labels==-1)], reachability[np.where(labels==-1)],color=colors[-1],alpha=0.3)
 
 # %%
 
-fig,ax=plt.subplots(1,1,figsize=(30,10))
-ax.plot(np.arange(0,len(X),1),clusterer.reachability_[clusterer.ordering_])
-re_his=clusterer.reachability_[:-1]
+# =============================================================================
+# fig,ax=plt.subplots(1,1,figsize=(30,10))
+# ax.plot(np.arange(0,len(X),1),clusterer.reachability_[clusterer.ordering_])
+# re_his=clusterer.reachability_[:-1]
+# =============================================================================
 # ax.hist(clusterer.reachability_[1:],bins=700)
 # %%
 re_his=clusterer.reachability_[:-1]
 # %%
 
-fig,ax=plt.subplots(1,1,figsize=(30,10))
-ax.scatter(np.arange(0,len(X),1),clusterer.core_distances_[clusterer.ordering_])
-re_his=clusterer.reachability_[:-1]
+# =============================================================================
+# fig,ax=plt.subplots(1,1,figsize=(30,10))
+# ax.scatter(np.arange(0,len(X),1),clusterer.core_distances_[clusterer.ordering_])
+# re_his=clusterer.reachability_[:-1]
+# =============================================================================
 # ax.hist(clusterer.reachability_[1:],bins=700)
 
 #%% NOw we use the knee on the reachability plot to set the max value fot epsilon
-X_stad = StandardScaler().fit_transform(X)
-# X_stad = X
+# =============================================================================
+# X_stad = StandardScaler().fit_transform(X)
+# # X_stad = X
+# 
+# # clusterer = hdbscan.HDBSCAN(min_cluster_size=samples, min_samples=min_cor,).fit(X_stad)
+# clusterer = OPTICS(min_samples=5, xi=0.7, metric='euclidean', max_eps=rodilla).fit(X_stad)
+# 
+# 
+# l=clusterer.labels_
+# 
+# n_clusters = len(set(l)) - (1 if -1 in l else 0)
+# print('Number of cluster with min_samples=%s: %s'%(samples,n_clusters))
+# n_noise=list(l).count(-1)
+# 
+# u_labels = set(l)
+# 
+# colors=[plt.cm.rainbow(i) for i in np.linspace(0,1,len(set(l)))]# Returns a color for each cluster. Each color consists in four number, RGBA, red, green, blue and alpha. Full opacity black would be then 0,0,0,1
+# #colors=np.array(colors)
+# # for i in range(len(colors)):
+# #     colors[i][-1]=prob[i]
+# =============================================================================
 
-# clusterer = hdbscan.HDBSCAN(min_cluster_size=samples, min_samples=min_cor,).fit(X_stad)
-clusterer = OPTICS(min_samples=5, xi=0.7, metric='euclidean', max_eps=rodilla).fit(X_stad)
-
-
-l=clusterer.labels_
-
-n_clusters = len(set(l)) - (1 if -1 in l else 0)
-print('Number of cluster with min_samples=%s: %s'%(samples,n_clusters))
-n_noise=list(l).count(-1)
-
-u_labels = set(l)
-
-colors=[plt.cm.rainbow(i) for i in np.linspace(0,1,len(set(l)))]# Returns a color for each cluster. Each color consists in four number, RGBA, red, green, blue and alpha. Full opacity black would be then 0,0,0,1
-#colors=np.array(colors)
-# for i in range(len(colors)):
-#     colors[i][-1]=prob[i]
-
-# This plots each cluster poit with its corresponding colour
-
-for k in range(len(colors)):
-    if list(u_labels)[k] == -1:
-        colors[k]=[0,0,0,0.1]
-     
-colores_index=[]
-
-for c in u_labels:
-    cl_color=np.where(l==c)
-    colores_index.append(cl_color)
-
-fig, ax = plt.subplots(1,1,figsize=(8,8))
-
-# for i in range(n_clusters):
-for i in range(len(set(l))):
-    # fig, ax = plt.subplots(1,1,figsize=(10,10))
-    # ax.set_title('Cluster #%s'%(i+1))
-    ax.scatter(X[:,0][colores_index[i]],X[:,1][colores_index[i]], color=colors[i],s=50)
-ax.set_ylabel(r'$\mu_{\delta}$ [mas/yr]', fontsize=16)
-ax.set_xlabel(r'$\mu_{\alpha}$ [mas/yr]', fontsize=16)    
-
-ax.set_xlim(-10, 10)
-ax.set_ylim(-10,10)    
-ax.invert_xaxis()
-
-
-
-
+# =============================================================================
+# # This plots each cluster poit with its corresponding colour
+# 
+# for k in range(len(colors)):
+#     if list(u_labels)[k] == -1:
+#         colors[k]=[0,0,0,0.1]
+#      
+# colores_index=[]
+# 
+# for c in u_labels:
+#     cl_color=np.where(l==c)
+#     colores_index.append(cl_color)
+# 
+# fig, ax = plt.subplots(1,1,figsize=(8,8))
+# 
+# # for i in range(n_clusters):
+# for i in range(len(set(l))):
+#     # fig, ax = plt.subplots(1,1,figsize=(10,10))
+#     # ax.set_title('Cluster #%s'%(i+1))
+#     ax.scatter(X[:,0][colores_index[i]],X[:,1][colores_index[i]], color=colors[i],s=50)
+# ax.set_ylabel(r'$\mu_{\delta}$ [mas/yr]', fontsize=16)
+# ax.set_xlabel(r'$\mu_{\alpha}$ [mas/yr]', fontsize=16)    
+# 
+# ax.set_xlim(-10, 10)
+# ax.set_ylim(-10,10)    
+# ax.invert_xaxis()
+# 
+# =============================================================================
 
 
 
