c912689aeadc6ee223c574a4e94796fc84efe027
Alvaro Martinez <amartinez@iaa.es>
2022-05-02 14:04:51 +0200

    Addded to the list in case same cluster is commun with more than one group

--- same_clusters_diff_MS.py
+++ same_clusters_diff_MS.py
@@ -53,19 +53,21 @@ for e in range(len(all_np)):
     ra, dec = all_np[e,0], all_np[e,1]
     new_all_np = np.delete(all_np,e,0)
     repeat = np.where((ra == new_all_np[:,0])&(dec == new_all_np[:,1]))
+    print(len(repeat),len(repeat[0]))
     if len(repeat[0]>0):
-        print(e, repeat[0])
-        print('Group %0.f, cluster %0.f and Group %0.f, cluster %0.f '
-              %(all_np[e][-2],all_np[e][-1],new_all_np[repeat][0][-2],new_all_np[repeat][-1][-1]))
-        indices.append((all_np[e][-2],all_np[e][-1],new_all_np[repeat][0][-2],new_all_np[repeat][-1][-1]))
-# %
+        print(e, repeat[0])    
+        for c in range(len(repeat)):
+            print('Group %0.f, cluster %0.f and Group %0.f, cluster %0.f '
+                  %(all_np[e][-2],all_np[e][-1],new_all_np[repeat][0][-2],new_all_np[repeat][-1][-1]))
+            indices.append((all_np[e][-2],all_np[e][-1],new_all_np[repeat][c][-2],new_all_np[repeat][c][-1]))
+    # %
 
 # %%
 indices_set = set(indices)
 indices_np = np.array(list(indices_set))
-
-np.savetxt(pruebas + '%scommon_clusters.txt'%(search),indices_np,fmt='%.0f',header = 'Group, cluster ---> Group, cluster') 
+sorted_array = indices_np[np.argsort(indices_np[:, 0])]
+np.savetxt(pruebas + '%scommon_clusters.txt'%(search),sorted_array,fmt='%.0f',header = 'Group, cluster ---> Group, cluster') 
 # %%
-
+print(repeat[0])
     
     
