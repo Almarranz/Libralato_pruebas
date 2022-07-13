1d91a672098ea99f8bdc714d780f3a974e6b5de1
Alvaro Martinez <amartinez@iaa.es>
2022-07-13 20:59:12 +0200

    Plotting simulated cluster and isochrme and some data

--- Arches_Hosek_kernel_H-Ks.py
+++ Arches_Hosek_kernel_H-Ks.py
@@ -86,60 +86,29 @@ epm_gal = SkyCoord(ra  = arches['ra*']*u.arcsec+center_arc.ra,dec = arches['dec'
 pme_lim = 0.4
 valid_epm = np.where((epm_gal.pm_l_cosb.value < pme_lim)&(epm_gal.pm_b.value < pme_lim))
 arches=arches[valid_epm]
+
 # %%
-# =============================================================================
-# Here we are going to match with GNS
-# =============================================================================
-Aks_gns = pd.read_fwf(gns_ext + 'central.txt', sep =' ',header = None)
+hos_coord = SkyCoord(ra  = arches['ra*']*u.arcsec+center_arc.ra,dec = arches['dec']*u.arcsec+ center_arc.dec)
+hos_gal = SkyCoord(ra = hos_coord.ra, dec = hos_coord.dec, frame = 'icrs').galactic
+pmra, pmdec = arches['pm_ra*']*u.mas/u.yr, arches['pm_dec']*u.mas/u.yr
 
-# %
-AKs_np = Aks_gns.to_numpy()
-center = np.where(AKs_np[:,6]-AKs_np[:,8] > 1.3)
-AKs_center =AKs_np[center]
-# %
-gns_coord = SkyCoord(ra=AKs_center[:,0]*u.degree, dec=AKs_center[:,2]*u.degree,frame = FK5,equinox ='J2000')
-
-
-# %% I cosider a math if the stars are less than 1 arcsec away 
-hos_coor = SkyCoord(ra  = arches['ra*']*u.arcsec+center_arc.ra,dec = arches['dec']*u.arcsec+ center_arc.dec)
-idi, d2d, d3d = hos_coor.match_to_catalog_sky(gns_coord)
-is_match = np.where(d2d<1*u.arcsec)
-
-hos_match_coord=hos_coor[is_match]
-hos_match = arches[is_match[0]]
-gns_match = AKs_center[idi[is_match]]
-# %%
-hos_match_gal = SkyCoord(ra = hos_match_coord.ra, dec = hos_match_coord.dec, frame = 'icrs').galactic
-pmra, pmdec = hos_match['pm_ra*']*u.mas/u.yr, hos_match['pm_dec']*u.mas/u.yr
-
-columnas = len(hos_match.columns)
+columnas = len(arches.columns)
 if columnas < 26:
-    hos_match.add_column(hos_match_coord.ra,name='ra_abs',index=0)
-    hos_match.add_column(hos_match_coord.dec,name='dec_abs',index=1)
-    hos_match.add_column(hos_match_gal.l,name='l_abs',index=2)
-    hos_match.add_column(hos_match_gal.b,name='b_abs',index=3)
-    pm_gal = SkyCoord(ra  = hos_match['ra_abs'] ,dec = hos_match['dec_abs'], pm_ra_cosdec = pmra, pm_dec = pmdec,frame = 'icrs').galactic
+    arches.add_column(hos_coord.ra,name='ra_abs',index=0)
+    arches.add_column(hos_coord.dec,name='dec_abs',index=1)
+    arches.add_column(hos_gal.l,name='l_abs',index=2)
+    arches.add_column(hos_gal.b,name='b_abs',index=3)
+    pm_gal = SkyCoord(ra  = arches['ra_abs'] ,dec = arches['dec_abs'], pm_ra_cosdec = pmra, pm_dec = pmdec,frame = 'icrs').galactic
     pml, pmb = pm_gal.pm_l_cosb, pm_gal.pm_b
-    hos_match.add_column(pml.value,name='pm_l',index=4)
-    hos_match.add_column(pmb.value,name='pm_b',index=5)
+    arches.add_column(pml.value,name='pm_l',index=4)
+    arches.add_column(pmb.value,name='pm_b',index=5)
 elif columnas == 26:
-    print('ra and dec already added to Hoseck data: \n',hos_match.columns)
+    print('ra and dec already added to Hoseck data: \n',arches.columns)
 # %%
-print(hos_match.columns)
+print(arches.columns)
 # %%
-#Checking the matching, you can delete these tree lines
-fig, ax = plt.subplots(1,2, figsize = (20,10))
-ax[0].scatter(hos_match['ra_abs'], hos_match['dec_abs'])
-ax[0].scatter(gns_match[:,0],gns_match[:,2],color = 'r',s=1)
-ax[1].scatter(hos_match['F127M']-hos_match['F153M'], hos_match['F153M'])
-ax[1].invert_yaxis()
-# %%
-# =============================================================================
-# This is for checking that the stars in both catalog are match-sorted
-# you could erase this,  but you know you will check this fact in the future
-# =============================================================================
-tc =200
-print((gns_match[tc,0]-hos_match['ra_abs'][tc])*u.deg.to(u.arcsec))
+
+
 #%%
 clustered_by = 'all_color'#TODO
 # clustered_by = 'all'#TODO
@@ -147,13 +116,13 @@ samples_dist=7
 # %%
 #here we generate the kernel simulated data 
 
-colorines = hos_match['F127M']-hos_match['F153M']
-pml_kernel, pmb_kernel = gaussian_kde(hos_match['pm_l']), gaussian_kde(hos_match['pm_b'])
-l_kernel, b_kernel = gaussian_kde(hos_match['l_abs'].value), gaussian_kde(hos_match['b_abs'].value)
+colorines = arches['F127M']-arches['F153M']
+pml_kernel, pmb_kernel = gaussian_kde(arches['pm_l']), gaussian_kde(arches['pm_b'])
+l_kernel, b_kernel = gaussian_kde(arches['l_abs'].value), gaussian_kde(arches['b_abs'].value)
 color_kernel = gaussian_kde(colorines)
 # %%
-pml, pmb = hos_match['pm_l'],hos_match['pm_b']
-l,b = hos_match['l_abs'].value,  hos_match['b_abs'].value
+pml, pmb = arches['pm_l'],arches['pm_b']
+l,b = arches['l_abs'].value,  arches['b_abs'].value
 if clustered_by == 'all_color':
     X = np.array([pml,pmb,l,b,colorines]).T
     X_stad = StandardScaler().fit_transform(X)
@@ -226,8 +195,8 @@ ax.set_ylabel('N')
 # =============================================================================
 # DBSCAN part
 # =============================================================================
-epsilon = eps_av
-clustering = DBSCAN(eps = epsilon, min_samples=samples_dist).fit(X_stad)
+
+clustering = DBSCAN(eps = eps_av, min_samples=samples_dist).fit(X_stad)
 
 l_c=clustering.labels_
 
@@ -255,29 +224,163 @@ elements_in_cluster=[]
 for i in range(len(set(l_c))-1):
     ax[0].scatter(pml[colores_index[i]], pmb[colores_index[i]],color=colors[i],zorder=3)
     # ax[1].scatter(l[colores_index[i]], b[colores_index[i]],color=colors[i],zorder=3)
-    ax[1].scatter(hos_match[colores_index[i]]['ra_abs'],hos_match[colores_index[i]]['dec_abs'],color=colors[i],zorder=3)
-    ax[2].scatter(hos_match['F127M'][colores_index[i]]-hos_match['F153M'][colores_index[i]],hos_match['F153M'][colores_index[i]],color=colors[i],zorder=13)
+    ax[1].scatter(arches[colores_index[i]]['ra_abs'],arches[colores_index[i]]['dec_abs'],color=colors[i],zorder=3,s=100)
+    # ax[1].scatter(gns_match[colores_index[i]][:,0],gns_match[colores_index[i]][:,2],color=colors[i],zorder=3,s=100)
+    ax[2].scatter(arches['F127M'][colores_index[i]]-arches['F153M'][colores_index[i]],arches['F153M'][colores_index[i]],color=colors[i],zorder=13)
     
 ax[0].scatter(pml[colores_index[-1]], pmb[colores_index[-1]],color=colors[-1],zorder=1)
+ax[0].set_xlabel(r'$\mathrm{\mu_{l} (mas\ yr^{-1})}$',fontsize =30) 
+ax[0].set_ylabel(r'$\mathrm{\mu_{b} (mas\ yr^{-1})}$',fontsize =30) 
 # ax[1].scatter(l[colores_index[-1]], b[colores_index[-1]],color=colors[-1],zorder=1)
-ax[2].scatter(hos_match['F127M'][colores_index[-1]]-hos_match['F153M'][colores_index[-1]],hos_match['F153M'][colores_index[-1]],color=colors[-1],zorder=1)
-# %
-# gns_cluster = gns_match[colores_index[0]]
+ax[1].scatter(arches[colores_index[-1]]['ra_abs'],arches[colores_index[-1]]['dec_abs'],color=colors[-1],zorder=3,s=100,alpha = 0.01)
+ax[1].set_xlabel('ra(deg)',fontsize =30) 
+ax[1].set_ylabel('dec(deg)',fontsize =30)
 
-# fix, ax = plt.subplots(1,1, figsize=(10,10))
-# ax.scatter(gns_cluster[colores_index[0]][:,0]['ra_abs'],hos_match[colores_index[0]]['dec'],s=20)
-# ax.scatter(gns_cluster[colores_index[0]][:,0],gns_cluster[colores_index[0]][:,2],color = 'red',s =5)
-
-
+ax[2].scatter(arches['F127M'][colores_index[-1]]-arches['F153M'][colores_index[-1]],arches['F153M'][colores_index[-1]],color=colors[-1],zorder=1)
+ax[2].set_xlabel('f127m-f153m',fontsize =30) 
+ax[2].set_ylabel('f153m',fontsize =30) 
 # %%
-# print(hos_match[colores_index[0]]['dec_abs'])
-ind_t=4
-fix, ax = plt.subplots(1,1, figsize=(10,10))
-for ind_t in range(5):
-    
-    ax.scatter(hos_match[colores_index[ind_t]]['ra_abs'],hos_match[colores_index[ind_t]]['dec_abs'],s=20)
+hos_cluster = arches[colores_index[0]]
+print(hos_cluster.columns)
+# %%
+# %%
+# =============================================================================
+# Here we are going to match with GNS
+# =============================================================================
+Aks_gns = pd.read_fwf(gns_ext + 'central.txt', sep =' ',header = None)
 
-print(len(set(l_c))-1)
+# %
+AKs_np = Aks_gns.to_numpy()
+center = np.where((AKs_np[:,6]-AKs_np[:,8] > 1.3) &(AKs_np[:,6]<90)&(AKs_np[:,8]<90))
+AKs_center =AKs_np[center]
+# %
+gns_coord = SkyCoord(ra=AKs_center[:,0]*u.degree, dec=AKs_center[:,2]*u.degree,frame = FK5,equinox ='J2000')
+
+
+# %% I cosider a math if the stars are less than 1 arcsec away 
+
+print(hos_cluster['ra_abs'])
+# %%
+# =============================================================================
+# Here we are going to select the core of the cluster buy selecting the DBSCAN
+# cluster scan around a distance equal to 'radio' of the center of the Arches or
+# quintuplet cluster
+# =============================================================================
+
+radio=1*u.arcsec#TODO
+hos_coor_clus = SkyCoord(ra = hos_cluster['ra_abs'], dec = hos_cluster['dec_abs'],frame = 'icrs')
+id1, id2, d2d,d3d = ap_coor.search_around_sky(SkyCoord(['17h45m50.4769267s'], ['-28d49m19.16770s'], frame='icrs'),hos_coor_clus, radio) if choosen_cluster =='Arches' else ap_coor.search_around_sky(SkyCoord(['17h46m15.13s'], ['-28d49m34.7s'], frame='icrs'),hos_coor_clus, radio)
+# dbs_clus, id_arc_dbs, d2d_db, d3d_db = ap_coor.search_around_sky(SkyCoord(['17h45m50.4769267s'], ['-28d49m19.16770s'], frame='icrs'),clus_gal, radio) if choosen_cluster =='Arches' else ap_coor.search_around_sky(SkyCoord(['17h46m15.13s'], ['-28d49m34.7s'], frame='icrs'),clus_gal, radio)
+
+fig, ax = plt.subplots(1,1,figsize =(10,10))
+ax.scatter(arches['ra_abs'],arches['dec_abs'],color ='k',alpha = 0.03)
+ax.scatter(hos_cluster[id2]['ra_abs'],hos_cluster[id2]['dec_abs'],color = 'g')
+
+hos_core = hos_cluster[id2]
+
+hos_core_coord = SkyCoord(ra = hos_core['ra_abs'], dec = hos_core['dec_abs'],frame ='icrs')
+idi, d2d, d3d = hos_core_coord.match_to_catalog_sky(gns_coord)
+is_match = np.where(d2d<1*u.arcsec)
+
+hos_core_match=hos_core[is_match]
+gns_core_match = AKs_center[idi[is_match]]
+
+#Checking the matching, you can delete these tree lines
+fig, ax = plt.subplots(1,3, figsize = (30,10))
+ax[0].scatter(arches['pm_l'],arches['pm_b'],color = 'k', alpha = 0.03)
+ax[0].scatter(hos_core_match['pm_l'],hos_core_match['pm_b'],color = 'lime')
+vel_txt = '\n'.join(('mul = %s, mub = %s'%(round(np.mean(hos_core_match['pm_l']),3), round(np.mean(hos_core_match['pm_b']),3)),
+                     '$\sigma_{mul}$ = %s, $\sigma_{mub}$ = %s'%(round(np.std(hos_core_match['pm_l']),3), round(np.std(hos_core_match['pm_b']),3)))) 
+vel_txt_all = '\n'.join(('mul = %s, mub = %s'%(round(np.mean(arches['pm_l']),3), round(np.mean(arches['pm_b']),3)),
+                     '$\sigma_{mul}$ = %s, $\sigma_{mub}$ = %s'%(round(np.std(arches['pm_l']),3), round(np.std(arches['pm_b']),3))))
+
+propiedades = dict(boxstyle='round', facecolor='lime' , alpha=0.3)
+propiedades_all = dict(boxstyle='round', facecolor=colors[-1], alpha=0.1)
+ax[0].text(0.05, 0.95, vel_txt, transform=ax[0].transAxes, fontsize=30,
+    verticalalignment='top', bbox=propiedades)
+ax[0].text(0.05, 0.15, vel_txt_all, transform=ax[0].transAxes, fontsize=20,
+    verticalalignment='top', bbox=propiedades_all)
+
+
+ax[1].scatter(hos_core_match['ra_abs'], hos_core_match['dec_abs'])
+ax[1].scatter(gns_core_match[:,0],gns_core_match[:,2],color = 'r',s=1)
+ax[1].scatter(arches['ra_abs'],arches['dec_abs'],color ='k',alpha = 0.03)
+# ax[2].scatter(hos_core_match['F127M']-hos_core_match['F153M'], hos_core_match['F153M'],zorder =3)
+# ax[2].scatter(arches['F127M']-arches['F153M'], arches['F153M'],zorder=1)
+ax[2].scatter(AKs_center[:,6]-AKs_center[:,8],AKs_center[:,8],zorder =1, color = 'k',s=0.1,alpha = 0.01)
+ax[2].scatter(gns_core_match[:,6]-gns_core_match[:,8],gns_core_match[:,6],zorder =3, color = 'lime')
+ax[2].set_xlim(1.2,4)
+ax[2].invert_yaxis()
+# %
+ext_cluster = []
+for ext in range(len(gns_core_match[:,18])):
+    ext_cluster.append(float(gns_core_match[ext,18]))
+
+
+AKs_core, dAKs_core = np.median(ext_cluster), np.std(ext_cluster)
+
+
+# %
+iso_dir = '/Users/amartinez/Desktop/PhD/Libralato_data/nsd_isochrones/'
+
+AKs_list1 =  np.arange(1.6,2.11,0.01)
+AKs_list = np.append(AKs_list1,0)#I added the 0 for the isochrones without extiction
+absolute_difference_function = lambda list_value : abs(list_value - AKs_core)
+AKs = min(AKs_list, key=absolute_difference_function)
+
+dist = 8200 # distance in parsec
+metallicity = 0.30 # Metallicity in [M/H]
+# # logAge_600 = np.log10(0.61*10**9.)
+if choosen_cluster =='Arches':
+    logAge = np.log10(0.0025*10**9.)#TODO
+elif choosen_cluster == 'Quintuplet':
+    logAge = np.log10(0.0048*10**9.)
+
+evo_model = evolution.MISTv1() 
+atm_func = atmospheres.get_merged_atmosphere
+red_law = reddening.RedLawNoguerasLara18()
+filt_list = ['hawki,J', 'hawki,H', 'hawki,Ks']
+
+iso =  synthetic.IsochronePhot(logAge, AKs, dist, metallicity=metallicity,
+                               evo_model=evo_model, atm_func=atm_func,
+                               red_law=red_law, filters=filt_list,
+                               iso_dir=iso_dir)
+
+imf_multi = multiplicity.MultiplicityUnresolved()
+
+
+massLimits = np.array([0.2, 0.5, 1, 120]) # Define boundaries of each mass segement
+powers = np.array([-1.3, -2.3, -2.3]) # Power law slope associated with each mass segment
+# my_imf = imf.IMF_broken_powerlaw(massLimits, powers, imf_multi)
+my_imf = imf.IMF_broken_powerlaw(massLimits, powers,multiplicity = None)
+
+ax[2].plot(iso.points['m_hawki_H'] - iso.points['m_hawki_Ks'], 
+                                      iso.points['m_hawki_Ks'], 'b-',   label='%.2f Myr'%(10**logAge/1e6),alpha =0.5)
+ax[2].legend()
+mass = 1*10**4.
+mass = 1 * mass
+dAks = round(dAKs_core,3)
+# dAks = 0.08
+cluster = synthetic.ResolvedClusterDiffRedden(iso, my_imf, mass,dAks)
+cluster_ndiff = synthetic.ResolvedCluster(iso, my_imf, mass)
+clus = cluster.star_systems
+clus_ndiff = cluster_ndiff.star_systems
+
+props = dict(boxstyle='round', facecolor='k', alpha=0.3)
+txt_AKs = '\n'.join(('AKs = %.2f'%(np.mean(ext_cluster)),
+                     'std_AKs = %.2f'%(np.std(ext_cluster))))
+ax[2].text(0.65, 0.50, txt_AKs, transform=ax[2].transAxes, fontsize=20,
+    verticalalignment='top', bbox=props)
+
+ax[2].scatter(clus['m_hawki_H']-clus['m_hawki_Ks'],clus['m_hawki_Ks'],color = 'r',alpha=0.1)
+ax[2].scatter(clus_ndiff['m_hawki_H']-clus_ndiff['m_hawki_Ks'],clus_ndiff['m_hawki_Ks'],color = 'k',alpha=0.1,s=1)
+
+
+
+
+
+
+
 
 
 
